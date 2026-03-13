5b9cbbae7e016b800da043d227957340be019e9e
Musa Shakib Khan <musa.khan@therapservices.net>
2026-03-13 23:30:04 +0600

    Added hyperparameters for models

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
@@ -23,12 +24,18 @@ def save_object(file_path, target_obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, Y_train, X_test, Y_test, models):
+def evaluate_model(X_train, Y_train, X_test, Y_test, models, params):
     try:
         report = {}
 
         for i in range(len(models)):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, param, cv = 3)
+            gs.fit(X_train, Y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, Y_train)
 
             Y_test_pred = model.predict(X_test)
