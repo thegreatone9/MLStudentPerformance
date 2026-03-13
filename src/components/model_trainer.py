2690176499b6116e3091ebb23c68110331387212
Musa Shakib Khan <musa.khan@therapservices.net>
2026-03-13 22:58:26 +0600

    Model Trainer coded

---
+++ src/components/model_trainer.py
@@ -0,0 +1,85 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+
+from src.utils import BASE_DIR, save_object, evaluate_model
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path: str = os.path.join(BASE_DIR, 'artifacts', "model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array):
+        logging.info("Iniating model training process")
+
+        try:
+            logging.info("Splitting training and testing input data")
+
+            X_train, Y_train, X_test, Y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            )
+
+            models = {
+                'Random Forest': RandomForestRegressor(),
+                'Decision Tree': DecisionTreeRegressor(),
+                'Gradient Boosting': GradientBoostingRegressor(),
+                'Linear Regression': LinearRegression(),
+                'K-Neighbors Regressor': KNeighborsRegressor(),
+                'XGBRegressor': XGBRegressor(),
+                'CatBoosting Regressor': CatBoostRegressor(verbose=False),
+                'AdaBoost Regressor': AdaBoostRegressor()
+            }
+
+            model_report: dict = evaluate_model(X_train = X_train, Y_train = Y_train, X_test = X_test, Y_test = Y_test, models = models)
+
+            ##Acquire the best model score from the report
+            best_model_score = max(sorted(model_report.values()))
+
+            ##Get best model name from report
+            best_model_name = list(model_report.keys()) [
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            best_model = models[best_model_name]
+
+            if best_model_score < 0.6:
+                logging.info("No best model found with score greater than 0.6.")
+                raise CustomException("No best model found with score greater than 0.6.", sys)
+            
+            logging.info("Best found model on both training and testing dataset.")
+
+            save_object(
+                file_path = self.model_trainer_config.trained_model_file_path,
+                target_obj = best_model
+            )
+
+            prediction = best_model.predict(X_test)
+
+            r2_score_val = r2_score(Y_test, prediction)
+
+            return (r2_score_val, prediction)
+
+        except Exception as e:
+            logging.info("Error occurred while initializing models.")
+            raise CustomException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import pandas as pd
 import dill
 
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -18,6 +19,25 @@ def save_object(file_path, target_obj):
 
         with open(file_path, "wb") as file_obj:
             dill.dump(target_obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_model(X_train, Y_train, X_test, Y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(X_train, Y_train)
+
+            Y_test_pred = model.predict(X_test)
+
+            test_model_score = r2_score(Y_test, Y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
